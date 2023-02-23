65d46fe0ea4f595bf654dd25fd91adbab760ef2a
bcarrara1 <123001830+bcarrara1@users.noreply.github.com>
2023-02-23 02:46:35 +0000

    done

--- Homework/Desmos.pl
+++ Homework/Desmos.pl
@@ -1,12 +1,23 @@
 #!/usr/bin/perl
 
-print("give me two numbers: ");
+print("give me two numbers: \n");
 $x1 = <>;
-print("one more number please:");
+print("one more number please:\n");
 $x2 = <>;
-print("what would you like to do today +,-,*,/,%,**");
+print("what would you like to do today add,sub,mult,div,mod,ex\n");
 $x = <>;
-chomp(x);
-if(x = +){
-    $sum = x1 + x2
+chomp($x);
+if($x eq "add"){
+    $sum = $x1 + $x2
+}elsif($x eq "sub"){
+    $sum = $x1 - $x2
+}elsif($x eq "mult"){
+    $sum = $x1 * $x2
+}elsif($x eq "div"){
+    $sum = $x1 / $x2
+}elsif($x eq "mod"){
+    $sum = $x1 % $x2
+}elsif($x eq "ex"){
+    $sum = $x1 ** $x2
 }
+print("$sum\n")
