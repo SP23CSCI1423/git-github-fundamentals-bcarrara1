d0349b0ca0c307e985bfc949cccfd41b79cbf625
bcarrara1 <123001830+bcarrara1@users.noreply.github.com>
2023-04-03 15:47:26 +0000

    done

--- Labs/Groceries.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-%groceries = (banana => 4, bread => 1, milk => 2, eggs => 3 );
-print("bananas are: $groceries{banana}\n");
-print("bread is: $groceries{bread}\n");
-print("milk is: $groceries{milk}\n");
-print("eggs are: $groceries{eggs}\n");
-$add = $groceries{banana} + $groceries{bread} + $groceries{milk} + $groceries{eggs};
-print("the price for your items are: $add\n");
--- Labs/rocketlaunch.pl
+++
@@ -1,8 +0,0 @@
-$num = 10;
-while($num != -1){
-    print ("$num\n");
-   $num = $num - 1;
-}
-if($num == 0){
-    print("Blast Off!\n")
-}
